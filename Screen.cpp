80d2cec6678c027a6b275300a112880b33d45ee5
Tate Purvis <tpurvis@una.edu>
2026-09-24 15:40:33 -0500

    Coded and tested Screen.cpp and Screen.h

---
+++ Screen.cpp
@@ -0,0 +1,27 @@
+#include "Screen.h"
+#include <iostream>
+
+namespace textui {
+    Screen::Screen(
+        const std::string& screen_title,
+        const std::vector<std::pair<char, std::string>>& menu_options
+    ) {
+        menu.set_title(screen_title);
+        for (auto& item_pair : menu_options) {
+            menu.add_item(item_pair.first, item_pair.second);
+        }
+    }
+
+    void Screen::show() {
+        std::cout << menu.get_title() << "\n";
+        for (const auto& item : menu) {
+            std::cout << item << "\n";
+        }
+    }
+
+    char Screen::get_choice() {
+        char choice;
+        std::cin >> choice;
+        return choice;
+    }
+}
--- Screen.h
+++ Screen.h
@@ -1,9 +1,21 @@
-#ifndef SCREEN_HPP
-#define SCREEN_HPP
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include <utility>
+#include <string>
+#include <vector>
+
+#include "Menu.h"
 
 namespace textui {
-    class Screen {
 
+    class Screen {
+        Menu menu;
+    public:
+        Screen(const std::string& screen_title, const std::vector<std::pair<char, std::string>>& menu_options);
+        void show();
+        char get_choice();
     };
-}
+    
+} // END TEXTUI NAMESPACE
 #endif
